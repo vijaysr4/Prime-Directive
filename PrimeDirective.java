21404d81008fe804ce052404c3749b89be239d81
Vijay <vijayvenkatesh2212@gmail.com>
2021-10-20 21:08:57 +0530

    prime num

---
+++ PrimeDirective.java
@@ -0,0 +1,39 @@
+// Import statement for arrayList
+import java.util.ArrayList;
+
+class PrimeDirective {
+  // method to determine the prime number 
+  public boolean isPrime(int number) {
+    if (number == 2) {
+      return true;
+    } else if (number < 2) {
+        return false;
+      }
+    for (int i = 2; i < number; i++) {
+      if (number % i == 0) {
+        return false;
+      }
+    }
+    return true;
+ }
+  
+  //method for ArrayList  
+  public ArrayList<Integer> onlyPrimes(int[] numbers) {
+    ArrayList<Integer> primes = new ArrayList<Integer>();
+      for (int number : numbers) {
+        if (isPrime(number)) {
+          primes.add(number);
+        }
+      }
+      return primes;
+}
+  //main function
+  public static void main(String[] args) {
+
+    PrimeDirective pd = new PrimeDirective();
+    int[] numbers = {6, 29, 28, 33, 11, 100, 101, 43, 89};
+
+    System.out.println(pd.onlyPrimes(numbers));
+
+  }
+}
